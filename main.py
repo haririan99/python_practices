0d800a333a1ed99d21c949f987374dc5bc342fc7
HH <haririan@gmail.com>
2023-09-04 18:48:47 +0330

    add lines welcome end msg

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 """ python practice By H.Haririan """
+print("Welcome")
 code = "India Romeo Alpha November - Alpha Bravo Alpha Delta"
 words = code.split()
 msg = ""
 for item in words:
     msg += item[0]
 print(msg)
+print("end")
